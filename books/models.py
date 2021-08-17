b592815baf7e6eca33a52af686b085a7ae54cbe7
Brian Harrison <brianharrison.2002@gmail.com>
2021-08-17 10:50:22 -0400

    Trying to commit

--- books/models.py
+++ books/models.py
@@ -8,3 +8,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        "Author", on_delete=models.CASCADE, related_name="books"
+    )
+    release_date = models.DateField()
+
+    def __repr__(self):
+        return f"<Book title={self.title}>"
+
+    def __str__(self) :
+        return self.title
+
+class Author (models.Model):
+    name = models.CharField(max_length=255)
+
+    def __repr__(self):
+        return f"<Author name={self.name}>"
+    def __str__(self):
+        return self.name
